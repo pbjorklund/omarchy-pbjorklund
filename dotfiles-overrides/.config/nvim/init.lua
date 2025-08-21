9203f6fdb674671d3f6142735e37ac6b02ad054f
Patrik Björklund <patrik@cognit.se>
2025-08-21 09:10:28 +0200

    Add nvim-notify

--- dotfiles-overrides/.config/nvim/init.lua
+++ dotfiles-overrides/.config/nvim/init.lua
@@ -588,6 +588,31 @@ require("lazy").setup({
       vim.keymap.set("n", "<leader>ms", "<cmd>MarkdownPreviewStop<cr>", { desc = "Markdown Preview Stop" })
     end,
   },
+  -- Better notifications
+  {
+    "rcarriga/nvim-notify",
+    config = function()
+      local notify = require("notify")
+      notify.setup({
+        background_colour = "#000000",
+        fps = 30,
+        icons = {
+          DEBUG = "",
+          ERROR = "",
+          INFO = "",
+          TRACE = "✎",
+          WARN = ""
+        },
+        level = 2,
+        minimum_width = 50,
+        render = "default",
+        stages = "fade_in_slide_out",
+        timeout = 3000,
+        top_down = true
+      })
+      vim.notify = notify
+    end,
+  },
 
   -- Precognition.nvim for movement hints
   {
