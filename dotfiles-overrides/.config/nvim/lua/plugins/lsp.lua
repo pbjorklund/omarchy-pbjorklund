9a2b90ca309c627c886fd92d013852cafb14f4c7
Patrik Björklund <patrik@cognit.se>
2025-08-21 14:26:58 +0200

    Remove nvim-cmp and luasnip dependencies from LSP config
    - Remove completion engine plugins since Copilot handles completion
    - Simplify LSP setup to focus on diagnostics and navigation
    - Remove cmp keybindings and configuration

--- dotfiles-overrides/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles-overrides/.config/nvim/lua/plugins/lsp.lua
@@ -3,12 +3,6 @@ return {
   dependencies = {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
   },
   config = function()
     -- Mason setup
@@ -19,11 +13,9 @@ return {
 
     -- LSP configuration
     local lspconfig = require("lspconfig")
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
     -- Lua LSP
     lspconfig.lua_ls.setup({
-      capabilities = capabilities,
       settings = {
         Lua = {
           runtime = {
@@ -43,52 +35,17 @@ return {
     })
 
     -- TypeScript LSP
-    lspconfig.ts_ls.setup({
-      capabilities = capabilities,
-    })
+    lspconfig.ts_ls.setup({})
 
     -- Python LSP
-    lspconfig.pyright.setup({
-      capabilities = capabilities,
-    })
+    lspconfig.pyright.setup({})
 
     -- LSP key mappings
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
       callback = function(ev)
         local opts = { buffer = ev.buf }
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
       end,
-    })
-
-    -- Completion setup
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-      }, {
-        { name = "buffer" },
-        { name = "path" },
-      }),
     })
   end,
 }
